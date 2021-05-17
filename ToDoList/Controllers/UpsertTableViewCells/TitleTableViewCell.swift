25a8aa8fbde08db996d5507c99da93c42229526d
snamiki1212 <snamiki1212@gmail.com>
2021-05-16 21:05:04 -0700

    Add check feature

--- ToDoList/Controllers/UpsertTableViewCells/TitleTableViewCell.swift
+++ ToDoList/Controllers/UpsertTableViewCells/TitleTableViewCell.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class TitleTableViewCell: UITableViewCell, UITextFieldDelegate {
-    let checkIconLabel: UILabel = {
-        let label = UILabel()
-        label.text = "✅"
-        label.setContentHuggingPriority(.required, for: .horizontal)
-        return label
+    let checkIcon: UIButton = {
+        let item = UIButton()
+        item.setContentHuggingPriority(.required, for: .horizontal)
+        return item
     }()
+    
+    private func updateCheckIcon(){
+        checkIcon.setTitle(checkIcon.isSelected ? "✅" : "☑️", for: .normal)
+    }
+    
+    @objc func onClickCheck(){
+        checkIcon.isSelected.toggle()
+        updateCheckIcon()
+    }
 
     let field: UITextField = {
         let field = UITextField()
@@ -23,10 +31,13 @@ class TitleTableViewCell: UITableViewCell, UITextFieldDelegate {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        // checkIcon
+        updateCheckIcon()
+        checkIcon.addTarget(self, action: #selector(onClickCheck), for: .touchUpInside)
         
-        let hStack = UIStackView(arrangedSubviews: [checkIconLabel, field])
+        // hstack
+        let hStack = UIStackView(arrangedSubviews: [checkIcon, field])
         self.contentView.addSubview(hStack)
-        
         hStack.translatesAutoresizingMaskIntoConstraints = false
         hStack.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         hStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
